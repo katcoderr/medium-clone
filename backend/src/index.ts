7676d007c3b232b39903bb080e2923a5f11f5b83
Kartik <kkartiktummawar@gmail.com>
2024-10-24 13:21:30 +0530

    Handlers init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,25 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+  return c.text('Hello')
 })
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello')
+})
+
 
 export default app
