0355c3f32f9442d0b3cf4840a378f1796ac7e36d
Kartik <kkartiktummawar@gmail.com>
2024-11-05 08:35:07 +0530

    Auth Integrated

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,8 @@
 import { SignupInput } from "@katcoder/medium-common"
 import { ChangeEvent, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import { BACKEND_URL } from "../config"
 
 export const Auth = ({type} : {type : "signup" | "signin"})=>{
     const [postInputs, setPostInputs] = useState<SignupInput>({
@@ -9,6 +11,20 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
         password : ""
     })
     
+    const navigate = useNavigate()
+
+    async function sendRequest(){
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,postInputs)
+            const jwt = response.data.jwt
+            localStorage.setItem("token", jwt)
+            navigate("/blogs")
+        } catch (e) {
+            alert("Error!")
+        }
+        
+    }
+
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div>
@@ -23,13 +39,13 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
             </div>
             </div>
             <div>
-        <LabelledInput label="Name" placeholder="John Doe" onChange={(e)=>{
+        {type === "signup" ? <LabelledInput label="Name" placeholder="John Doe" onChange={(e)=>{
             setPostInputs({
                 ...postInputs,
                 name : e.target.value
             })
 
-        }} />
+        }} /> : null }
 
 <LabelledInput label="Username" placeholder="username@example.com" onChange={(e)=>{
             setPostInputs({
@@ -47,7 +63,7 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
 
         }} />
 
-<button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-full mt-2">{type === "signin" ? "Sign In" : "Sign Up"}</button>
+<button onClick={sendRequest} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-full mt-2">{type === "signin" ? "Sign In" : "Sign Up"}</button>
 
         </div>
         </div>
